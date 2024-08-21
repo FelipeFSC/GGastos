3fdf318552744db6868be3b68d9728aeb67c5b34
FelipeFsc <felipefsc.04@gmail.com>
2024-08-21 00:07:26 -0300

    Adicionando modelo inicial da tela de cartoes e contas

--- GGastos-web/src/app/accounts/accounts.component.ts
+++ GGastos-web/src/app/accounts/accounts.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-accounts',
-  templateUrl: './accounts.component.html',
-  styleUrls: ['./accounts.component.css']
+    selector: 'app-accounts',
+    templateUrl: './accounts.component.html',
+    styleUrls: ['./accounts.component.css']
 })
 export class AccountsComponent implements OnInit {
 
-  constructor() { }
+    data: any = [
+        {
+            nome: "Banco 1",
+            image: "Foto 1",
+        },
+        {
+            nome: "Banco BCT",
+            image: "Foto 1",
+        },
+        {
+            nome: "Nu Bancos",
+            image: "Foto 1",
+        },
+        {
+            nome: "Piracantuba Bancos",
+            image: "Foto 1",
+        },
+    ]
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 
 }
--- GGastos-web/src/app/cards/cards.component.ts
+++ GGastos-web/src/app/cards/cards.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-cards',
-  templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+    selector: 'app-cards',
+    templateUrl: './cards.component.html',
+    styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
 
-  constructor() { }
+    data: any = [
+        {
+            nome: "Banco 1",
+            image: "Foto 1",
+        },
+        {
+            nome: "Banco BCT",
+            image: "Foto 1",
+        },
+        {
+            nome: "Nu Bancos",
+            image: "Foto 1",
+        },
+        {
+            nome: "Piracantuba Bancos",
+            image: "Foto 1",
+        },
+    ]
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 
 }
